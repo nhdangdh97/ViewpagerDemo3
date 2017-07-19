5c1b98583facca2f5ffc5f6ae32767b3173ebc33
nhdangdh97 <nhdang1997@gmail.com>
2017-07-19 22:53:54 +0700

    done

--- app/src/main/java/com/example/nhdangdh/viewpagerdemo/fragment/AndroidFragment.java
+++ app/src/main/java/com/example/nhdangdh/viewpagerdemo/fragment/AndroidFragment.java
@@ -6,8 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.example.nhdangdh.viewpagerdemo.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by nhdangdh on 7/19/2017.
@@ -15,9 +21,34 @@ import com.example.nhdangdh.viewpagerdemo.R;
 
 public class AndroidFragment extends Fragment {
 
+    Button btnAdd;
+    EditText edtAdd;
+    ListView lv;
+
+    ArrayAdapter<String> adapter;
+    ArrayList<String> arrData;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_android, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_android, container, false);
+        lv = (ListView) rootView.findViewById(R.id.lv);
+        arrData = new ArrayList<String>();
+        arrData.add("Android");
+        arrData.add("IOS");
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, arrData);
+        lv.setAdapter(adapter);
+
+        edtAdd = (EditText) rootView.findViewById(R.id.edtAdd);
+        btnAdd = (Button) rootView.findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrData.add(edtAdd.getText()+"");
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        return rootView;
     }
 }
--- app/src/main/java/com/example/nhdangdh/viewpagerdemo/main/MainActivity.java
+++ app/src/main/java/com/example/nhdangdh/viewpagerdemo/main/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.nhdangdh.viewpagerdemo.R;
 import com.example.nhdangdh.viewpagerdemo.adapter.PageApdater;
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     ViewPager viewPager;
     TabLayout tabLayout;
+    Button btnAdd;
+    EditText edtAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private void addControls() {
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        edtAdd = (EditText) findViewById(R.id.edtAdd);
     }
 
     private void addEvents() {
